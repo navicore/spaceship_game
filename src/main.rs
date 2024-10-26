ad90137cda2bba712579ae078c65f50d7d5b475d
Ed Sweeney <ed@onextent.com>
2024-10-26 06:52:49 -0700

    ep1

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,14 @@ struct Velocity {
     y: f32,
 }
 
+#[derive(Resource, Debug)]
+struct GameState {
+    pub is_playing: bool,
+}
+
 fn main() {
     App::new()
+        .insert_resource(GameState { is_playing: true })
         .add_systems(Startup, spawn_spaceship)
         .add_systems(Update, (update_positions, print_position))
         .add_plugins(DefaultPlugins)
@@ -31,9 +37,12 @@ fn update_positions(mut query: Query<(&Velocity, &mut Position)>) {
     }
 }
 
-fn print_position(query: Query<(Entity, &Position)>) {
+fn print_position(query: Query<(Entity, &Position)>, game_state: Res<GameState>) {
     // log the entity ID and position
     for (entity, position) in query.iter() {
-        info!("Entity {:?} is at position {:?}", entity, position);
+        info!(
+            "Entity {:?} is at position {:?} with state {:?}",
+            entity, position, game_state.is_playing
+        );
     }
 }
