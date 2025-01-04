f4eb1351254f159872f9e24694a87581af28eeca
Ed Sweeney <ed@onextent.com>
2025-01-03 19:00:50 -0800

    font

--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, text::TextBounds};
+
+#[derive(Debug, Component)]
+struct GameOverText;
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
 pub enum GameState {
@@ -18,6 +21,8 @@ impl Plugin for StatePlugin {
             (
                 game_state_input_events,
                 transition_to_game_restart.run_if(in_state(GameState::GameOver)),
+                display_game_over_text.run_if(in_state(GameState::GameOver)),
+                remove_game_over_text.run_if(in_state(GameState::GameRestart)),
             ),
         );
     }
@@ -35,6 +40,45 @@ pub fn game_state_input_events(
             GameState::GameOver => (),
             GameState::GameRestart => (),
         }
+    }
+}
+
+// TODO: this does not work
+// TODO: this does not work
+// TODO: this does not work
+// TODO: this does not work
+// TODO: this does not work
+fn display_game_over_text(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+    let box_size = Vec2::new(300.0, 200.0);
+    let box_position = Vec2::new(0.0, -250.0);
+    // Demonstrate text wrapping
+    let slightly_smaller_text_font = TextFont {
+        font,
+        font_size: 35.0,
+        ..default()
+    };
+    commands
+        .spawn((
+            Sprite::from_color(Color::srgb(0.25, 0.25, 0.75), box_size),
+            Transform::from_translation(box_position.extend(0.0)),
+        ))
+        .with_children(|builder| {
+            builder.spawn((
+                Text2d::new("Game Over - Press <Enter> to play again"),
+                slightly_smaller_text_font.clone(),
+                TextLayout::new(JustifyText::Left, LineBreak::WordBoundary),
+                // Wrap text in the rectangle
+                TextBounds::from(box_size),
+                // ensure the text is drawn on top of the box
+                Transform::from_translation(Vec3::Z),
+            ));
+        });
+}
+
+fn remove_game_over_text(mut commands: Commands, query: Query<Entity, With<GameOverText>>) {
+    for entity in query.iter() {
+        commands.entity(entity).despawn();
     }
 }
 
